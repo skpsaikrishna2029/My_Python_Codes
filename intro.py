99de97e9fc63512efff79d43a5d6a33937cfa272
skpsaikrishna2029 <skp.saikrishna2029@gmail.com>
2025-07-16 19:17:33 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,36 @@
+# # print("Hello World")
+# name = "Sai"
+# age = 17
+# price = 25.99
+# print (name)
+# print(age)
+# print(price)
+# print("my name is :",name)
+# age2=age
+# print(age2)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# a=2
+# b=5
+# sum=a+b
+# print(sum)
+# type conversion
+# a=2
+# b=4.25
+# sum=a+b# 2.0+4.25 =>6.25
+# print(sum)
+# a=2
+# b=4.25
+# sum=a+b
+# print(sum)
+# 
+# first=int(input("enter first:"))
+# second=int(input("enter second:"))
+# print("sum=", first+second)
+# side=float(input("enter square side:"))
+# print("area=",side**2)
+# # 
+# a=int(input("enter firs:"))
+# b=int(input("enter second:"))
+# print(a>=b)
