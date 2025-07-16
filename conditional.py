4f15ede09c333cb5d4944d721df01cb37b4db095
skpsaikrishna2029 <skp.saikrishna2029@gmail.com>
2025-07-16 19:18:28 +0530

    Update conditional.py

--- conditional.py
+++ conditional.py
@@ -1,33 +1,33 @@
-# str = "this is a string.\nWe are creating it in python."
-# print(str)
-# 
-# str = "my name is sai.\ti am in 17 years old."
-# print(str)
-# str1 ="sai"
-# str2 = "krishna"
-# final_str =str1+str2
-# print(final_str)
-# str1 = "saikrishna"
-# len1 = len (str1)
-# print(len1)
-# str2 = "panda"
-# len2 = len(str2)
-# print(len2)
-# final_str = str1+" "+str2
-# print(final_str)
-# print(len(final_str))
-# str = "saikrishna panda"
-# ch = str[5]
-# print(ch)
-# str = "saikrishna panda"
-# print(str[5:12])
-# str = "sheep"
-# print(str[-4:-2])
-# str = "i am a student"
-# print(str.endswith("ent"))
-# str = "i am a student"
-# print(str.endswith("ude"))
-# str = "i am a boy."
-# print(str.capitalize())
-# str = "ia am saikrishna"
-# print(str.replace("i","I"))
+str = "this is a string.\nWe are creating it in python."
+print(str)
+
+str = "my name is sai.\ti am in 17 years old."
+print(str)
+str1 ="sai"
+str2 = "krishna"
+final_str =str1+str2
+print(final_str)
+str1 = "saikrishna"
+len1 = len (str1)
+print(len1)
+str2 = "panda"
+len2 = len(str2)
+print(len2)
+final_str = str1+" "+str2
+print(final_str)
+print(len(final_str))
+str = "saikrishna panda"
+ch = str[5]
+print(ch)
+str = "saikrishna panda"
+print(str[5:12])
+str = "sheep"
+print(str[-4:-2])
+str = "i am a student"
+print(str.endswith("ent"))
+str = "i am a student"
+print(str.endswith("ude"))
+str = "i am a boy."
+print(str.capitalize())
+str = "ia am saikrishna"
+print(str.replace("i","I"))
